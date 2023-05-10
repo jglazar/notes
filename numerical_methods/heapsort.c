bb8396cb2119d8340dbeae2352a6d05c909d9f7a
James Glazar <36523155+jglazar@users.noreply.github.com>
2023-05-10 17:09:11 -0400

    add 3rd ed. heapsort implementation

--- numerical_methods/heapsort.c
+++ numerical_methods/heapsort.c
@@ -7,62 +7,72 @@
 #include <stdio.h>
 
 #define HSIZE 10
+#define SWAP(T, i, j) { T TMP = *i; *i = *j; *j = TMP; }
+#define LEFT(i) (2*i)
+#define RIGHT(i) (2*i+1)
+#define PARENT(i) (i/2)
 
-static int a[HSIZE+1] = {0, 3, 2, 5, 4, 7, 6, 9, 10, 1, 8};
-static int lower = (HSIZE / 2) + 1;
-static int upper = HSIZE;
-static int curr_val;
+static int a[HSIZE] = {0, 3, 2, 5, 4, 7, 6, 9, 8, 1};
 
-void decrement_phase()
+void print_arr()
 {
-    // hiring phase
-    if (lower > 1)
+    for (int i = 0; i < HSIZE; i++)
     {
-        curr_val = a[--lower];
-        return;
+        printf("%d ", a[i]);
     }
-    // retire/promotion phase
-    curr_val = a[upper];
-    a[upper] = a[1]; // retire top man into end
-    if (--upper == 1)
-    {
-        a[1] = curr_val;
-        // <send heapsort abort signal here>
-    }
-    return;
+    printf("\n");
 }
 
+// sift-down a[l] to maintain heap
+// l and r determine the “left” and “right” range of the sift-down.
+void sift_down(const int l, const int r, )
+{
+    if (j > r) { return; }
+    int j = RIGHT(l), jold = l;
+    while (j <= r)
+    {
+        if (j < r && a[j] < a[j+1]) { ++j; } // set j to better child
+        if (a[j] <= a[l]) { break; } // found proper level
+        a[jold] = a[j];
+        jold = j;
+        j=2*j+1; 
+    } 
+    a[jold]=a[l];
+}
+
+// indexing starts at zero for this one
 void heapsort()
 {
-    // lower is decremented to 1 in hiring phase
-    // upper is decremented to 1 in retire/promotion phase
-    while (1)
+    // i = left range of the sift-down (element to be sifted)
+    // decremented in hiring (heap creation) phase.
+    int i;
+    for (i = HSIZE/2 - 1; i >= 0; --i)
     {
-        decrement_phase();
-       // sift curr_val to proper place 
-       int i = lower;
-       int j = lower * 2;
-       while (j <= upper)
-       {
-           // set j to better child
-           if (j < upper && a[j] < a[j+1]) { ++j; } 
-           // check for demotion
-           if (curr_val < a[j])
-           {
-               a[i] = a[j];
-               j += (i = j);
-           } 
-           // otherwise, we're on curr_val's level and terminate 
-           // sift-down by setting j to upper + 1
-           else { j = upper + 1; }
-       }
-       // finally, put curr_val in its place
-       a[i] = curr_val;
+        sift_down(i, HSIZE - 1);
+    }
+    // right range of the sift-down 
+    // decremented in retirement-and-promotion (heap selection) phase.
+    for (i = HSIZE - 1; i > 0; i--)
+    {
+        // Clear space at end array, then retire top of the heap into it.
+        SWAP(int, &a[0], &a[i]); 
+        sift_down(0,i-1); 
     }
 }
 
+/*
+typedef struct Phase
+{
+    int lower_idx;
+    int upper_idx;
+} Phase;
 
-void print(int *a)
+void print_phase(Phase p)
+{
+    printf("lower %d, upper %d\n", p.lower_idx, p.upper_idx);
+}
+
+void print_arr()
 {
     for (int i = 1; i < HSIZE+1; i++)
     {
@@ -72,10 +82,77 @@ void print(int *a)
 }
 
 
+Phase decrement_phase(Phase p)
+{
+    // hiring phase
+    if (p.lower_idx > 1)
+    {
+        --p.lower_idx; 
+        return p;
+    }
+    // retire/promotion phase
+    --p.upper_idx;
+    return p;
+}
+
+
+int get_current_val(Phase p)
+{
+    // hiring phase
+    if (p.lower_idx > 1)
+    {
+        return a[p.lower_idx];
+    }
+    // retire/promotion phase
+    int curr_val = a[p.upper_idx+1];
+    a[p.upper_idx+1] = a[1]; // retire top man into end
+    return curr_val;
+}
+
+int sift_down(int curr_val, Phase p)
+{
+    // sift curr_val to proper place 
+    int i = p.lower_idx;
+    int j = p.lower_idx * 2;
+    while (j <= p.upper_idx)
+    {
+       // set j to better child
+       if (j < p.upper_idx && a[j] < a[j+1]) { ++j; } 
+       // check for demotion
+       if (curr_val < a[j])
+       {
+           a[i] = a[j];
+           j += (i = j);
+       } 
+       // otherwise, we're on curr_val's level 
+       else { break; }
+    }
+    return i;
+}
+
+void heapsort()
+{
+    int curr_val, place;
+    Phase phase = {(HSIZE/2)+1, HSIZE};
+    while (1)
+    {
+        phase = decrement_phase(phase);
+        curr_val = get_current_val(phase);
+        if (phase.lower_idx == 1 && phase.upper_idx == 1)
+        {
+            a[1] = curr_val;
+            return;
+        }
+        place = sift_down(curr_val, phase);
+        a[place] = curr_val;
+    }
+}
+*/
+
 int main()
 {
-    print(a);
-    heapsort(a);
-    print(a);
+    print_arr();
+    heapsort();
+    print_arr();
     return 0;
 }
